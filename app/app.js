3f7f47a4038e8d33ff9429eb7f80c63c35d6eec9
dieu-vu <vuphuongdieu212@gmail.com>
2021-11-26 14:09:05 +0200

    correct app.js template

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
 //import necessary libraries:
 const cors = require('cors');
-const express = require('express);
+const express = require('express');
 //TODO: error handling, passport, thumbnails
 
 //start new express application:
@@ -12,8 +12,11 @@ const port = 3000;
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true});
+app.use(express.urlencoded({extended: true}));
 
+app.get('/', (req,res) => {
+	res.send("Welcome to Nutitivity site")
+});
 //place holder for route modules:
 //var authRoute = require('./routes/authRoute.js');
 //var userRoute = require('./routes/userRoute.js');
@@ -23,4 +26,4 @@ app.use(express.urlencoded({extended: true});
 //app.use('/user', userRoute);
 //app.use('/activity', activityRoute);
 
-app.listen(port, ()) => console.log(`Example app listening on port ${port}`));
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
