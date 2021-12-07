71858b1964735eb07cad4d7310b5b149776820ca
Dieu Vu <dieu.vu@metropolia.fi>
2021-12-07 11:31:28 +0200

    display profile for logged in user in frontend OK

--- app/frontend/scripts/user-profile.js
+++ app/frontend/scripts/user-profile.js
@@ -9,7 +9,8 @@ const userInfo = document.querySelector('#user-info');
 const userEditForm = document.querySelector('#edit-profile-form');
 
 // // get user data from session storage
-// const user = JSON.parse(sessionStorage.getItem('user'));
+const loggedInUser = JSON.parse(sessionStorage.getItem('user'));
+const loggedInUserId = loggedInUser.user_id;
 
 // open and close side menu for editing user info:
 const openMenu = () => {
@@ -38,8 +39,7 @@ const createUserPic = (user) => {
     imgContainer.classList.add('container-image');
 
     const img = document.createElement('img');
-    img.src = 'http://placedog.net/200/300'
-    // img.src = url + '/thumbnails/user/' + user.filename;
+    img.src = url + '/thumbnails/user/' + user.filename;
     img.alt = user.name;
     img.classList.add('user-image');
 
@@ -68,44 +68,53 @@ const createActivityStack = (activities, headerText) => {
     ul.classList.add = 'image-stack';
 
     ul.innerHTML = '';
-    activities.forEach((activity) => {
-        const img = document.createElement('img');
-        img.src = 'http://placedog.net/200/300';
-        //img.src = url + '/thumbnails/' + activity.filename;
-        img.alt = activity.name;
-        img.classList.add('activity-image');
-        img.classList.add('trans');
+
+    if (activities.length !==0){
+        activities.forEach((activity) => {
+            const img = document.createElement('img');
+            img.src = 'http://placedog.net/200/300';
+            //img.src = url + '/thumbnails/' + activity.filename;
+            img.alt = activity.name;
+            img.classList.add('activity-image');
+            img.classList.add('trans');
 
 
-        img.addEventListener('click', () => {
-            location.href = 'activity.html?id=?' + activity.activity_id;
+            img.addEventListener('click', () => {
+                location.href = 'activity.html?id=?' + activity.activity_id;
+            });
+
+            const figure = document.createElement('figure').appendChild(img);
+            
+            const h2 = document.createElement('h2');
+            h2.innerHTML = activity.name;
+
+            const p1 = document.createElement('p');
+            p1.innerHTML = `Location: ${activity.location}`;
+
+            const p2 = document.createElement('p');
+            p2.innerHTML = activity.description;
+
+            const li = document.createElement('li');
+            li.classList.add('single-image');
+
+            li.appendChild(h2);
+            li.appendChild(figure);
+            li.appendChild(p1);
+            li.appendChild(p2);
+            
+            ul.appendChild(li);
+
+            stack.appendChild(header);
+            stack.appendChild(ul);
+
         });
-
-        const figure = document.createElement('figure').appendChild(img);
-        
-        const h2 = document.createElement('h2');
-        h2.innerHTML = activity.name;
-
-        const p1 = document.createElement('p');
-        p1.innerHTML = `Location: ${activity.location}`;
-
-        const p2 = document.createElement('p');
-        p2.innerHTML = activity.description;
-
-        const li = document.createElement('li');
-        li.classList.add('single-image');
-
-        li.appendChild(h2);
-        li.appendChild(figure);
-        li.appendChild(p1);
-        li.appendChild(p2);
-        
-        ul.appendChild(li);
-
+    } else {
+        const noActivityMessage = document.createElement('p');
+        noActivityMessage.innerHTML = "No activities at the moment...";
         stack.appendChild(header);
-        stack.appendChild(ul);
-
-    });
+        stack.appendChild(noActivityMessage);
+        stack.style.color = '#FFFFFF80';
+    }
 };
 
 // get profile data:
@@ -114,11 +123,12 @@ const getProfile = async () => {
         // TODO: implement with passport/session
         const fetchOptions = {
             headers: {
-                Authorization: 'Bearer' + sessionStorage.getItem('token'),
+                Authorization: 'Bearer ' + sessionStorage.getItem('token'),
             },
         };
-        //TODO: update this route when finish login
-        const response = await fetch(url + '/user/1');
+       
+        console.log('LOGGED IN USER', loggedInUserId);
+        const response = await fetch(url + `/user/${loggedInUserId}`, fetchOptions);
         const user = await response.json();
         console.log('USER', user);
 
@@ -129,12 +139,11 @@ const getProfile = async () => {
 
         console.log('participate', user.participateActivity);
     } catch (e) {
-        console.log(e.message);
+        console.log(e);
     }
 };
 
 getProfile();
-console.log('Current user id LOG', current_user_id);
 
 // Submit user profile edit form:
 userEditForm.addEventListener('submit', async (editEvent) => {
@@ -147,17 +156,16 @@ userEditForm.addEventListener('submit', async (editEvent) => {
           delete data[prop];
         }
     }
-
     const fetchOptions = {
         method: 'PUT',
         header: {
             'Content-Type': 'application/json',
-            // Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+            Authorization: 'Bearer ' + sessionStorage.getItem('token'),
         },
         body: JSON.stringify(data),
     };
-    //backend will handle user_id with login passport for put method
 
+    //backend will handle user_id with login passport for put method
     const response = await fetch(url + '/user', fetchOptions);
     const json = await response.json();
     if (json.error) {alert(json.error.message)};
