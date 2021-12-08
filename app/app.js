65660c7cfba7959591488d192582de8e74bd212e
myxmxm <myxmxm@gmail.com>
2021-12-08 14:17:35 +0200

    update

--- app/app.js
+++ app/app.js
@@ -22,7 +22,7 @@ app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static('./backend/uploads'));
+app.use(express.static('uploads'));
 app.use('./thumbnails', express.static('thumbnails'));
 
 app.use('/auth', authRoute);
