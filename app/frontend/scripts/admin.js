5f92fbb5031d575584a0204ad00c05c353d7e00c
Dieu Vu <dieu.vu@metropolia.fi>
2021-12-09 17:23:33 +0200

    display user list with delete button on admin page

--- app/frontend/scripts/admin.js
+++ app/frontend/scripts/admin.js
@@ -12,10 +12,16 @@ const loggedInUserId = JSON.parse(sessionStorage.getItem('user')).user_id;
 
 // Create tabs for displaying user and activity list:
 const tabs = document.querySelector('.tab-panel');
+const itemList = document.querySelector('.item-list');
+
 const createTabs = (node) => {
     let tabs = Array.from(node.children).map ( node => {
         let button = document.createElement('button');
         button.textContent = node.getAttribute('data-tabname');
+        // let listDiv = document.createElement('div');
+        // listDiv.classList.add = node.getAttribute('data-tabname');
+        // listDiv.innerHTML = createUserList(userNameList);
+
         let tab = {node, button};
         button.addEventListener('click', () => selectTab(tab));
         return tab;
@@ -29,12 +35,14 @@ const createTabs = (node) => {
         for (let tab of tabs) {
             let selected = tab === selectedTab;
             tab.node.style.display = selected ? "inline-block" : "none";
+            // tab.listDiv.style.display = selected ? "block" : "none";
             tab.button.style.color = selected ? "#004155" : "#fcfcfc";
         }
     }
     selectTab(tabs[0]);
 };
 createTabs(tabs);
+
 
 // get user list for User tab
 const getAllUsers = async () => {
@@ -48,6 +56,9 @@ const getAllUsers = async () => {
         const response = await fetch(url + `/user/`, fetchOptions);
         const users = await response.json();
         console.log('USERS RETRIEVED', users.length);
+        //const userNameList = users.map( (user) => { return user.name; });
+        createUserList(users);
+
     } catch (e) {
         console.log(e);
     }
@@ -62,33 +73,105 @@ const getAllActitivities = async () => {
             },
         };
         console.log('LOGGED IN USER', loggedInUserId);
-        const response = await fetch(url + `/activity/`, fetchOptions);
-        const users = await response.json();
-        console.log('ACTIVITIES RETRIEVED', users.length);
-        // userNameList
+        const response = await fetch(url + '/activity/', fetchOptions);
+        const activities = await response.json();
+        console.log('ACTIVITIES RETRIEVED', activities.length);
+        console.log('ACTIVITIES RETRIEVED', activities);
+
+        createActivityList(activities);
 
     } catch (e) {
         console.log(e);
     }
 };
+
+
+// create list for displaying user in user tab:
+const createUserList = (users) => {
+    const ul = document.createElement('ul');
+    ul.classList.add('tab-list');
+    ul.innerHTML = '';
+
+    if (users.length !== 0) {
+        users.forEach((user) => {
+            const name = document.createElement('p');
+            name.classList.add('item-name');
+            name.innerHTML = `${user.name}`;
+    
+            const li = document.createElement('li');
+            li.appendChild(name);
+
+            const deleteButton = document.createElement('button');
+            deleteButton.innerHTML = 'Delete';
+            deleteButton.classList.add('button');
+            deleteButton.style.display = "inline";
+            deleteButton.addEventListener('click', async () => {
+                const fetchOptions = {
+                  method: 'DELETE',
+                  headers: {
+                    Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+                  },
+                };
+                try {
+                  const response = await fetch(url + '/user/' + user.user_id, fetchOptions);
+                  const json = await response.json();
+                  console.log('delete response', json);    
+                } catch (e) {
+                  console.log(e.message);
+                }
+            });
+
+            li.appendChild(deleteButton);
+
+            ul.appendChild(li);
+        });
+    }
+    itemList.appendChild(ul);    
+};
+
+// create list for displaying activity in activity tab:
+const createActivityList = (activities) => {
+    const ul = document.createElement('ul');
+    ul.classList.add('tab-list');
+    ul.innerHTML = '';
+
+    if (activities.length !== 0) {
+        activities.forEach((activity) => {
+            const name = document.createElement('p');
+            name.classList.add('item-name');
+            name.innerHTML = `${activity.activity}`;
+    
+            const li = document.createElement('li');
+            li.appendChild(name);
+
+            console.log("activity name", name.innerHTML);
+            const deleteButton = document.createElement('button');
+            deleteButton.innerHTML = 'Delete';
+            deleteButton.classList.add('button');
+            deleteButton.style.display = "inline";
+            deleteButton.addEventListener('click', async () => {
+                const fetchOptions = {
+                  method: 'DELETE',
+                  headers: {
+                    Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+                  },
+                };
+                try {
+                  const response = await fetch(url + '/activity/' + activity.id, fetchOptions);
+                  const json = await response.json();
+                  console.log('delete response', json);        
+                } catch (e) {
+                  console.log(e.message);
+                }
+            });
+            li.appendChild(deleteButton);
+
+            ul.appendChild(li);
+        });
+    }
+    itemList.appendChild(ul);    
+};
+
 getAllUsers();
 getAllActitivities();
 
-// create list for displaying user in user tab:
-const createUserList = (userList) => {
-    const ul = document.createElement('ul');
-    ul.classList.add('user-list');
-    ul.innerHTML = '';
-
-    if (userList.length !== 0) {
-        const name = document.createElement('p');
-        name.classList.add('user-name');
-
-        const li = document.createElement('li');
-        li.appendChild(name);
-
-        ul.appendChild(li);
-    }
-
-    
-}
